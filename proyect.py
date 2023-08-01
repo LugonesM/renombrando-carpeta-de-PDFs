d3bc8f0a36a2034db11b5f247460c59580470964
Mel <54716002+LugonesM@users.noreply.github.com>
2023-08-01 14:23:16 -0300

    Update proyect.py

--- proyect.py
+++ proyect.py
@@ -1,24 +1,23 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python3
 
-import PyPDF2
+from PyPDF2 import PdfReader
 import os
-
 
 def extract_words_from_pdf(pdf_path):
     extracted_words = []
 
     with open(pdf_path, 'rb') as pdf_file:
-        pdf_reader = PyPDF2.PdfFileReader(pdf_file)
-        num_pages = pdf_reader.numPages
+        pdf_reader = PdfReader(pdf_file)
+        num_pages = len(pdf_reader.pages)
 
         for page_num in range(num_pages):
-            page = pdf_reader.getPage(page_num)
+            page = pdf_reader.pages[page_num]
             text = page.extract_text()
             words = text.split()
             extracted_words.extend(words)
 
-    return extracted_words[:5]  # Extrae solo las primeras 5 palabras
+    return extracted_words[:5]  # Extract only the first 5 words
 
 
 def process_words(words):
@@ -29,38 +28,31 @@ def process_words(words):
         processed_words += word + " "
     return processed_words.strip()
 
-
-
-
 def main():
+    folder_path = 'recibos'  # Folder where all the PDF receipts are stored
 
-    folder_path = 'recibos' #carpeta donde estan todos los recibos en pdf
-
-    # Itera por todos los pdfs de la carpeta
+    # Iterate over all the PDFs in the folder
     for filename in os.listdir(folder_path):
-    
-        # toma el full path del pdf
+        # Get the full path of the PDF
         pdf_path = os.path.join(folder_path, filename)
 
-        # extrae solo las primeras 5 palabras que es donde se encuentran los nombres
+        # Extract only the first 5 words, which should contain the name
         extracted_words = extract_words_from_pdf(pdf_path)
 
-        # Procesa las palabras para que sean SOLO el nombre 
+        # Process the words to get only the name
         processed_words = process_words(extracted_words)
 
-        # Generata un string con el nombre que sera el nombre del pdf
+        # Generate a string with the name, which will be the new PDF name
         new_filename = processed_words + ".pdf"
         new_pdf_path = os.path.join(folder_path, new_filename)
 
-        # Renombra el pdf
+        # Rename the PDF
         os.rename(pdf_path, new_pdf_path)
 
-        print("Renamed PDF file: ", new_filename)
-            
-
-
+        print("Renamed PDF file:", new_filename)
 
 if __name__ == "__main__":
-    main()   
-    
+    main()
+
+
     
